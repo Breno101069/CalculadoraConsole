63799b3461af174c1bdac3720da4efce0569481c
Breno Balassoni <brenobalassoni5@gmail.com>
2022-08-24 00:07:23 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraConsole/Classes/Calculadora.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraConsole.Classes
+{
+    public class Calculadora
+    {
+        public double Resultado { get; set; }
+        public void Soma(double valor1, double valor2)
+        {
+            this.Resultado = valor1 + valor2;
+            Console.WriteLine($"O valor da soma é: {Resultado}");
+        }
+
+        public void Subtrair(double valor1, double valor2)
+        {
+            this.Resultado = valor1 - valor2;
+            Console.WriteLine($"O valor da subtração é: {Resultado}");
+        }
+
+        public void Dividir(double valor1, double valor2)
+        {
+            this.Resultado = valor1 / valor2;
+            Console.WriteLine($"O valor da divisão é: {Resultado}");
+        }
+
+        public void Multiplicacao(double valor1, double valor2)
+        {
+            this.Resultado = valor1 * valor2;
+            Console.WriteLine($"O valor da multiplicação é: {Resultado}");
+        }
+    }
+}
---
+++ CalculadoraConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using CalculadoraConsole.Classes;
+using Newtonsoft.Json;
+
+namespace CalculadoraConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+            calculadora.Soma(10, 10.3);
+        }
+    }
+}
